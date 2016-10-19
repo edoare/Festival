43fb16fa6949c7b31b9247b77780408bbecc372e
btssio <btssio@btssio-VirtualBox>
2016-10-19 16:52:16 +0200

    codage TestAttribution

--- test/TestAttribution.php
+++ test/TestAttribution.php
@@ -2,15 +2,24 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Groupe Test</title>
+        <title>Test Attribution</title>
     </head>
     <body>
         <?php
+        use modele\metier\Attribution;
+        use modele\metier\TypeChambre;
         use modele\metier\Groupe;
+        use modele\metier\Etablissement;
+        
         require_once __DIR__ . '/../includes/autoload.php';
-        echo "<h2>Test unitaire de la classe métier Groupe</h2>";
-        $objet = new Groupe("g999","les Joyeux Turlurons","général Alcazar","Tapiocapolis" ,25,"San Theodoros","N");
-        var_dump($objet);
+        echo "<h2>Test Attribution</h2>";
+        
+        $typeChambre = new TypeChambre("C1", "1 lit");
+        $groupe = new Groupe("G015", "La Tuque Bleue", NULL, NULL, 8, "Québec", "O");
+        $etab = new Etablissement("0352072M", "Institution Saint-Malo Providence", "2 rue du collège BP 31863", "35418", "Saint-Malo", "0299407474", NULL, 1, "Monsieur", "Durand", "Pierre");
+        $attribution = new Attribution($etab,$typeChambre,$groupe, 15);        
+        
+        var_dump($attribution);
         ?>
     </body>
 </html>
--- test/TestOffre.php
+++ test/TestOffre.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>EtablissementDAO : test</title>
+        <title>Test Offre</title>
     </head>
 
     <body>
@@ -17,8 +17,8 @@ require_once __DIR__ . '/../includes/autoload.php';
         echo "TestOffre";
         $etab = new Etablissement('0123456A', 'Collège St Pierre ', '3, avenue de la Borderie BP 32', '35404', 'Paramé', '0299560159', NULL, '1', 'Madame', 'Truc', 'Muche');
         $typeChambre = new typeChambre('C7', '2 à 4 lits');
-        $objet = new Offre($etab, $typeChambre, '8');
-        var_dump($objet);
+        $offre = new Offre($etab, $typeChambre, '8');
+        var_dump($offre);
         ?> 
     </body>
 </html>
